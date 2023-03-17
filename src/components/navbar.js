7db8ba716df3a5db4cf926293607c00c1bfefc27
isiko404 <isihd.ko@gmail.com>
2023-03-17 17:48:02 +0100

    Fix Navbar Main-Page Link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import LoginBtn from '@/components/login-btn'
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-        <a class="navbar-brand" href="#">Zitate-App</a>
+        <Link class="navbar-brand" href="/">Zitate-App</Link>
         <LoginBtn />  
         </div>
         </nav>
